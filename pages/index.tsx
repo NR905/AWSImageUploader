13f6d98ce79c0d2392761e990f9411c541051e52
Niha <niha.rana@atmecs.com>
2023-09-15 23:36:33 +0530

    img upload app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,92 @@
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { Authenticator } from '@aws-amplify/ui-react';
+import awsExports from '../src/aws-exports';
+import { Amplify, Auth, Storage } from 'aws-amplify';
+import ImageUploading from 'react-images-uploading';
+import { StorageImage } from '@aws-amplify/ui-react-storage';
+import { FileUploader } from '@aws-amplify/ui-react';
+import { ScrollView, defaultDarkModeOverride, Button, Flex, Heading, ThemeProvider, ColorMode, Card, ToggleButtonGroup, ToggleButton, Text } from '@aws-amplify/ui-react';
+
+Amplify.configure({ ...awsExports });
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const [imageKeyList , setImageKeyList] = useState<string[]>([])
+  const onUploadSuccess = (event:any) => {
+    console.log(event)
+    getImageList()
+  }
+
+  const getImageList = async () => {
+    const files = await Storage.list('', { level: 'private' })
+    const keys = files.results.map(f => f.key as string)
+    if (keys.length > 0) {
+      setImageKeyList(keys)
+    }
+  }
+
+  const handleDelete = async (key:string) => {
+    await Storage.remove(key, { level: 'private' })
+    getImageList()
+  }
+
+  useEffect(() => {
+    getImageList()
+  }, [])
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <Flex direction='column' height='100vh' backgroundColor={"slate"}>
+      <Flex justifyContent={"center"}>
+      <Image src={"https://www.niharana.com/assets/aiu.jepg"} className='self-center' alt={"Niharana"} width={400} height={200} />
+      <Flex direction='column' justifyContent='center' alignItems='center'>
+      <Button
+          alignSelf={'center'}
+          variation='warning'
+          width={100}
+          onClick={() => Auth.signOut()}>
+          Sign out
+        </Button>
+      </Flex>
+
+      </Flex>
+
+      <Authenticator>
+        <Flex flex={1} justifyContent='space-between'>
+          <Flex direction='column' width='40%'>
+            <Text
+              textAlign='center'
+            >Image Uploader</Text>
+            <FileUploader
+              acceptedFileTypes={['image/*']}
+              accessLevel="private"
+              onSuccess={onUploadSuccess}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          </Flex>
+          <Flex direction='column' width='60%'>
+            <Text
+              textAlign='center'
+            >Image Gallery</Text>
+            <ScrollView maxHeight={"70vh"}>
+            {
+              imageKeyList && imageKeyList.map((key) => (
+                <Card key={key}  >
+                  <Flex direction={'column'}>
+                  <StorageImage key={key} alt={key} imgKey={key} accessLevel="private" />
+                  <Button width={100} variation='warning' onClick={() => handleDelete(key)}>Delete</Button>
+                  </Flex>
+                </Card>
+              ))
+            }
+            </ScrollView>
+          </Flex>
+        </Flex>
+        
+      </Authenticator>
+    </Flex>
   )
 }
